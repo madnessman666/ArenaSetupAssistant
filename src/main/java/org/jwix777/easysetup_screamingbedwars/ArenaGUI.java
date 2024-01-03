2aa7d8ea45095caf6a8a924af4970278167f5c26
jwix777 <69116290+jwix777alt@users.noreply.github.com>
2024-01-03 11:08:25 +0300

    Initial commit

--- src/main/java/org/jwix777/easysetup_screamingbedwars/ArenaGUI.java
+++ src/main/java/org/jwix777/easysetup_screamingbedwars/ArenaGUI.java
@@ -3,27 +3,16 @@ package org.jwix777.easysetup_screamingbedwars;
 import com.samjakob.spigui.buttons.SGButton;
 import com.samjakob.spigui.item.ItemBuilder;
 import com.samjakob.spigui.menu.SGMenu;
-import me.filoghost.holographicdisplays.api.hologram.Hologram;
-import me.filoghost.holographicdisplays.api.hologram.VisibilitySettings;
-import org.bukkit.Bukkit;
-import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.material.Wool;
 import org.screamingsandals.bedwars.api.BedwarsAPI;
 import org.screamingsandals.bedwars.api.game.Game;
 import org.screamingsandals.bedwars.api.game.GameStatus;
-import org.screamingsandals.bedwars.api.game.ItemSpawner;
-
-import java.util.List;
-
-import static org.jwix777.easysetup_screamingbedwars.EasySetup_ScreamingBedwars.holoapi;
 
 public class ArenaGUI implements Listener {
 
-    public List<Hologram> holos;
     public void open(Player player, String arenaName){
         SGMenu Menu = EasySetup_ScreamingBedwars.spiGUI.create("Arena: ".concat(arenaName), 3);
         Material toggleMaterial;
@@ -50,14 +39,10 @@ public class ArenaGUI implements Listener {
         });
         SGButton back = new SGButton(
                 new ItemBuilder(Material.BARRIER).name("Back").build()
-        ).withListener((InventoryClickEvent event) -> {
-            new ArenasGUI().open(player);
-        });
+        ).withListener((InventoryClickEvent event) -> new ArenasGUI().open(player));
         SGButton setSpawner = new SGButton(
                 new ItemBuilder(Material.MOB_SPAWNER).name("Set Spawner").build()
-        ).withListener((InventoryClickEvent event) -> {
-            new SpawnerGUI().open(player, arenaName);
-        });
+        ).withListener((InventoryClickEvent event) -> new SpawnerGUI().open(player, arenaName));
         Menu.addButton(toggle);
         Menu.addButton(setSpawner);
         Menu.addButton(back);
--- src/main/java/org/jwix777/easysetup_screamingbedwars/EasySetupGUI.java
+++ src/main/java/org/jwix777/easysetup_screamingbedwars/EasySetupGUI.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.defaults.BukkitCommand;
 
 public class EasySetupGUI implements CommandExecutor {
     @Override //переопределяем метод
